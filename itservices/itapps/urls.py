b84f3a6d40ab0b6537f3ed77148c4b8f221e9cf3
b5018299 <b5018299@my.shu.ac.uk>
2019-04-18 02:08:20 +0100

    Created home, about, contact us page. Set up the Product page but not functional. The admin interface is set up.

--- itservices/itapps/urls.py
+++ itservices/itapps/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('reviewApp.urls')), #refering to the urls.py file in the application 'reviewApp' folder
+    #path('productList/', include('reviewApp.urls')),
+    #path('productDetail/', include('reviewApp.urls')),
 ]
--- itservices/reviewApp/views.py
+++ itservices/reviewApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+	return render(request, 'reviewApp/home.html', {'title': 'Home'})
+def about(request):
+	return render(request, 'reviewApp/about.html', {'title': 'About'})
+def contact(request):
+	return render(request, 'reviewApp/contact.html', {'title': 'Contact'})
+def product(request):
+	return render(request, 'reviewApp/product.html', {'title': 'Product'})
+
+def product(request):
+	product_item= {
+		'product': products
+	}
+	return renter(request, 'product/product.html', product_item)
