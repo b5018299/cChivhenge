879d27bb4e8aae9ad3c718cffd5f1457be47efb5
b5018299 <b5018299@my.shu.ac.uk>
2019-04-23 19:08:38 +0100

    User can reset password with gmail email account

--- itservices/itapps/urls.py
+++ itservices/itapps/urls.py
@@ -19,14 +19,20 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from users import views as user_views #defining user_views as the views folder for users
 from django.contrib.auth import views as auth_views
+#from django.contrib.auth.views import password_reset, password_reset_done
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviewApp.urls')), #refering to the urls.py file in the application 'reviewApp' folder
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
+    #path('reset-password/', password_reset, name='reset_password'),
     #path('productList/', include('reviewApp.urls')),
     #path('productDetail/', include('reviewApp.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 	path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+	path('reset-password/', auth_views.PasswordResetView.as_view(template_name='users/reset-password.html'), name='password_reset'),
+	path('reset-password/done', auth_views.PasswordResetDoneView.as_view(template_name='users/reset-password-done.html'), name='password_reset_done'),
+	path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+	path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
