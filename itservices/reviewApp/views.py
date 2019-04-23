88f31b42aea46ccde1fdc283360ba8253e459009
b5018299 <b5018299@my.shu.ac.uk>
2019-04-23 22:47:41 +0100

    Created functional contact form which send an email to a gmail account

--- itservices/reviewApp/views.py
+++ itservices/reviewApp/views.py
@@ -1,5 +1,9 @@
 from django.urls import reverse
+from django.conf import settings
 from .models import Product, Review
+# from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -9,7 +13,24 @@ def home(request):
 def about(request):
 	return render(request, 'reviewApp/about.html', {'title': 'About'})
 def contact(request):
-	return render(request, 'reviewApp/contact.html', {'title': 'Contact'})
+	if request.method == 'POST':
+		name = request.POST['name']
+		emailAddress = request.POST['email']
+		message = request.POST['message']
+
+		subject = "Review Tech Contact"
+		#emailMessage = "%s %s is testing an email with a newline.  \nCheck out this link too: http://www.example.com" % (user.first_name, user.last_name)
+		emailMessage = " Full Name: %s \n Email Address: %s \n Message: %s" % (name, emailAddress, message)
+		from_string = "<support@example.com>"
+		# email_message = EmailMessage(subject, emailMessage, from_string, [user.email])
+		email_message = EmailMessage(subject, emailMessage, from_string, ['chishachivhenge@gmail.com'])
+		email_message.send()
+
+		# email = EmailMessage('Subject', 'Message: ' + message, to=['chishachivhenge@gmail.com'])
+		# email.send()
+		return render(request, 'reviewApp/home.html', {'title': 'Home'})
+	else:
+		return render(request, 'reviewApp/contact.html', {'title': 'Contact'})
 def product(request):
 	return render(request, 'reviewApp/product.html', {'title': 'Product'})
 
@@ -88,3 +109,14 @@ class DeleteReview(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 		if self.request.user == review.author:
 			return True
 		return False
+
+# def index(request):
+# 	if request.method == 'POST':
+# 		message = request.POST['message']
+
+# 		send_mail('Conract Form', #Email heading
+# 		message,
+# 		settings.EMAIL_HOST_USER,
+# 		['chishachivhenge@gmail.com'],
+# 		fail_silently=False)
+# 	return render(request, 'product')
